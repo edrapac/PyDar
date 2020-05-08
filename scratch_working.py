2f499f6d5173315405c1d6ac41d9493319806e8b
Emerson <emerson.drapac@outlook.com>
2020-05-08 03:51:13 +0100

    scratch script

---
+++ scratch_working.py
@@ -0,0 +1,29 @@
+#WORKING SERVO SCRIPT
+# You need to provide 5V of power if using the red servo!
+
+import time
+import wiringpi
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+
+# set #18 to be a PWM output
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+# divide down clock
+# This is what sets the pwm frequency, 50 Hz (typical servo) which is determined via pwmSetRange/pwmSetClock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+
+delay_period = 0.01
+
+while True:
+    for pulse in range(50, 250, 1):
+        print('sending pulse')
+        wiringpi.pwmWrite(18, pulse)
+        time.sleep(delay_period)
+    for pulse in range(250, 50, -1):
+        wiringpi.pwmWrite(18, pulse)
+        time.sleep(delay_period)
