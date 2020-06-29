46e95b2b01feab71a7f23bc71f19a86441495a12
edrapac <emerson.drapac@mnsu.edu>
2020-06-29 14:16:54 -0500

    added docker stuff

---
+++ Docker/app/index.php
@@ -0,0 +1,70 @@
+<?php
+
+function getOSInformation()
+ {
+     if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
+         return null;
+     }
+
+      $os         = shell_exec('cat /etc/os-release');
+     $listIds    = preg_match_all('/.*=/', $os, $matchListIds);
+     $listIds    = $matchListIds[0];
+
+      $listVal    = preg_match_all('/=.*/', $os, $matchListVal);
+     $listVal    = $matchListVal[0];
+
+      array_walk($listIds, function(&$v, $k){
+         $v = strtolower(str_replace('=', '', $v));
+     });
+
+      array_walk($listVal, function(&$v, $k){
+         $v = preg_replace('/=|"/', '', $v);
+     });
+
+      return array_combine($listIds, $listVal);
+ }
+$osInfo = getOSInformation();
+?>
+<!doctype html>
+<html lang=en>
+<head>
+    <meta charset=utf-8>
+    <title>Hello World from Docker-LA-P</title>
+
+    <style>
+        @import 'https://fonts.googleapis.com/css?family=Montserrat|Raleway|Source+Code+Pro';
+
+        body { font-family: 'Raleway', sans-serif; }
+        h2 { font-family: 'Montserrat', sans-serif; }
+        pre {
+            font-family: 'Source Code Pro', monospace;
+
+            padding: 16px;
+            overflow: auto;
+            font-size: 85%;
+            line-height: 1.45;
+            background-color: #f7f7f7;
+            border-radius: 3px;
+
+            word-wrap: normal;
+        }
+
+        .container {
+            max-width: 1024px;
+            width: 100%;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <section>
+            <pre>
+OS: <?php echo $osInfo['pretty_name']; ?><br/>
+Apache: <?php echo apache_get_version(); ?><br/>
+PHP Version: <?php echo phpversion(); ?><br/>
+            </pre>
+        </section>
+    </div>
+</body>
+</html>
---
+++ Docker/supporting_files/run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Prepare our container for initial boot.
+
+
+#######################################
+# Use sed to replace apache php.ini values for a given PHP version.
+# Globals:
+#   PHP_UPLOAD_MAX_FILESIZE
+#   PHP_POST_MAX_SIZE
+#   PHP_TIMEZONE
+# Arguments:
+#   $1 - PHP version i.e. 5.6, 7.3 etc.
+# Returns:
+#   None
+#######################################
+function replace_apache_php_ini_values () {
+    echo "Updating for $1"
+
+    sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+        -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php/$1/apache2/php.ini
+
+    sed -i "s/;date.timezone =/date.timezone = Europe\/London/g" /etc/php/$1/apache2/php.ini
+
+}
+if [ -e /etc/php/5.6/apache2/php.ini ]; then replace_apache_php_ini_values "5.6"; fi
+if [ -e /etc/php/$PHP_VERSION/apache2/php.ini ]; then replace_apache_php_ini_values $PHP_VERSION; fi
+
+#######################################
+# Use sed to replace cli php.ini values for a given PHP version.
+# Globals:
+#   PHP_TIMEZONE
+# Arguments:
+#   $1 - PHP version i.e. 5.6, 7.3 etc.
+# Returns:
+#   None
+#######################################
+function replace_cli_php_ini_values () {
+    sed -i  "s/;date.timezone =/date.timezone = Europe\/London/g" /etc/php/$1/cli/php.ini
+}
+if [ -e /etc/php/5.6/cli/php.ini ]; then replace_cli_php_ini_values "5.6"; fi
+if [ -e /etc/php/$PHP_VERSION/cli/php.ini ]; then replace_cli_php_ini_values $PHP_VERSION; fi
+
+sed -i "s/export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=staff/" /etc/apache2/envvars
+
+if [ -n "$APACHE_ROOT" ];then
+    rm -f /var/www/html && ln -s "/app/${APACHE_ROOT}" /var/www/html
+fi
+
+if [ -n "$VAGRANT_OSX_MODE" ];then
+    usermod -u $DOCKER_USER_ID www-data
+    groupmod -g $(($DOCKER_USER_GID + 10000)) $(getent group $DOCKER_USER_GID | cut -d: -f1)
+    groupmod -g ${DOCKER_USER_GID} staff
+else
+    # Tweaks to give Apache/PHP write permissions to the app
+    chown -R www-data:staff /var/www
+    chown -R www-data:staff /app
+fi
+
+exec supervisord -n
