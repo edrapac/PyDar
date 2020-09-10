b43454bea3d07dd7e47ce0cf8efef21577a9b8cd
edrapac <emerson.drapac@mnsu.edu>
2020-09-10 18:36:27 -0500

    channel hopping with bash script

--- scapyDev/channelHop.sh
+++ scapyDev/channelHop.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 # Loop to hop channels, needs to be run as root 
+set -eou pipefail
 
 while true;
-	channel=$(($RANDOM % 14+1))
-	do echo $channel;
-	sleep 1
+	channel=$(($RANDOM % 13+1))
+	do echo "Channel is now being set to: $channel"
+	iw dev IFACE_NAME set channel "$channel"
+	current_frequency=$(iwconfig IFACE_NAME | grep -o -E Frequency:[0-9].[0-9]+[[:space:]]GHz)
+	echo "Current interface operating frequency set to $current_frequency"
+	sleep 2
 	done;	
+
--- scapyDev/testSniffing.py
+++ scapyDev/testSniffing.py
@@ -1,9 +1,11 @@
-# Simple script to make sure that sniffing is working as intended. Change the wlan1mon iface to whatever iface you want. Yes, I know this can be done from scapy's interactive menu but the idea here is that we want to make sure that that functionality can be called from a python file with no issues
+# Simple script to make sure that sniffing is working as intended. Change the wlan1mon iface to whatever iface you want. 
+# Yes, I know this can be done from scapy's interactive menu but the idea here is that we want to make sure that that 
+# functionality can be called from a python file with no issues
 from scapy.all import *
 import sys
 
 try:
-    sniff(iface='wlan1mon',prn=lambda x: x.show())
+    sniff(iface='wlp4s0mon',prn=lambda x: x.show())
 except KeyBoardInterrupt:
     print('Keyboard interrupt detected, exiting now')
     sys.exit(0) #clean exit
