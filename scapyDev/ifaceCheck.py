5437478ac18cca5b49d2d18b1d403afcb643d237
edrap <edrap>
2020-09-14 16:27:17 +0100

    fix input checker

--- scapyDev/ifaceCheck.py
+++ scapyDev/ifaceCheck.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import re
+
+iface_reg = re.compile("wlan\S+")
 
 whoami = os.popen('whoami').read().rstrip() # os.popen added newlines, not sure why
 
@@ -9,4 +12,15 @@ if whoami != 'root': # we need root access to put the iface into monitor mode
 
 iw_output = os.popen('iw dev').read().rstrip()
 
-print(iw_output)
+monitor_iface = iw_output.replace('monitor', 'monitor <- WARNING: This interface is already in monitor mode')
+print(monitor_iface)
+result = re.findall(iface_reg,monitor_iface)
+for i in range(len(result)):
+    print(i,":",result[i],"\n")
+x= input("Please choose one of the following interfaces to put into monitor mode: ")
+
+try:
+    print("Attempting to now put %s in monitor mode" % (result[x]))
+except Exception as e:
+    print('Exception encountered, printing now')
+    print(e)
