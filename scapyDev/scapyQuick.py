e259422aaaea04bd149e0f3a87feb6515140eddf
Emerson Drapac <edrapac@netspi.com>
2020-09-21 21:12:30 -0500

    working on classes

--- scapyDev/scapyQuick.py
+++ scapyDev/scapyQuick.py
@@ -11,60 +11,65 @@ import argparse
 '''
 Define some globals
 '''
+class Scanner ():
+    def __init__(self):
+        self.data_frame = pd.DataFrame(columns=["SSID", "Signal Strength (dBm)"])
+        
+        self.data_frame.set_index("SSID", inplace=True)
 
-data_frame = pd.DataFrame(columns=["SSID", "Signal Strength (dBm)"])
-data_frame.set_index("SSID", inplace=True)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--iface',required=True)
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('--iface',required=True)
+        self.arguments = self.parser.parse_args()
 
-def channel_hop(ifacename):
-    while True:
+    def channel_hop(self):
+        while True:
+            try:
+                stream = os.system('./channelHop.sh '+self.arguments.iface)
+                time.sleep(2)
+            except KeyboardInterrupt:
+                print('Ctrl+c detected, shutting down')
+                sys.exit(1)
+            except Exception as e:
+                print('Non keyboard interrupt exception detected! Printing now')
+                print(e)
+    
+    def callback(packet):  # processes sniffed packets and calls the pdframe method
         try:
-            stream = os.system('./channelHop.sh '+ifacename)
-            time.sleep(2)
-        except KeyboardInterrupt:
-            print('Ctrl+c detected, shutting down')
-            sys.exit(1)
+            if packet.haslayer(Dot11):  # check if the packet has an 802.11 layer ie Wifi
+                if packet.type == 0 and packet.subtype == 8:  # here we start accessing 802.11 specific fields
+                    ssid = str(packet.info, 'UTF8')
+                    dbm = str(packet.dBm_AntSignal)
+                    pdframe(ssid, dbm)
         except Exception as e:
-            print('Non keyboard interrupt exception detected! Printing now')
             print(e)
-def callback(packet):  # processes sniffed packets and calls the pdframe method
-    try:
-        if packet.haslayer(Dot11):  # check if the packet has an 802.11 layer ie Wifi
-            if packet.type == 0 and packet.subtype == 8:  # here we start accessing 802.11 specific fields
-                ssid = str(packet.info, 'UTF8')
-                dbm = str(packet.dBm_AntSignal)
-                pdframe(ssid, dbm)
-    except Exception as e:
-        print(e)
-        pass  # bad packet or something, best to just pass it
+            pass  # bad packet or something, best to just pass it
+    def getIface(self):
+        return self.arguments.iface
+
+    def printFrame(self):
+        while True:
+            print(self.data_frame.to_string())
+            # unfortunately no better way to do this than to feed clear directly to the OS to clear stdout
+            time.sleep(0.5)
+            os.system("clear")
 
 
-def printFrame(q):
-    while True:
-        print(data_frame.to_string())
-        # unfortunately no better way to do this than to feed clear directly to the OS to clear stdout
-        time.sleep(0.5)
-        os.system("clear")
-
-
-def pdframe(ssid, dbm):  # Updates the pdframe to contain the newest beacon frame and associated information
-    SSID = ssid
-    data_frame.loc[SSID] = (dbm)
+    def pdframe(ssid, dbm):  # Updates the pdframe to contain the newest beacon frame and associated information
+        SSID = ssid
+        data_frame.loc[SSID] = (dbm)
 
 
 
 if __name__ == "__main__":
-    arguments = parser.parse_args()
-    print(arguments.iface)
-    x = threading.Thread(target=printFrame, args=(data_frame,))
+    newScanner = Scanner()
+    x = threading.Thread(target=newScanner.printFrame)
     x.daemon = True  # daemonize the thread otherwise it will be dependent on the sniffing
     x.start()
     
-    y = threading.Thread(target=channel_hop,args=(arguments.iface,)) #call channel hopping on a monitor mode interface as passed by the command
+    y = threading.Thread(target=newScanner.channel_hop) #call channel hopping on a monitor mode interface as passed by the command
     y.daemon = True
     y.start()
 
 
-    sniff(iface=arguments.iface, prn=callback) # main thread of execution begin sniffing as last task
+    sniff(iface=newScanner.getIface, prn=newScanner.callback) # main thread of execution begin sniffing as last task
